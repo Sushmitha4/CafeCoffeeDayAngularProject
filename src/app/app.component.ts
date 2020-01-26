ae02652bcaee75d76bab12028330399093b12216
Sushmitha <sushmithaspoojary4@gmail.com>
2020-01-26 08:22:11 +0530

    second commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,18 +9,5 @@ import * as firebase from "firebase";
 export class AppComponent implements OnInit {
   title = "cafeCoffeeDay";
 
-  ngOnInit() {
-    // Your web app's Firebase configuration
-    var firebaseConfig = {
-      apiKey: "AIzaSyCN81K4tvf1p4jfob9vI2RfXy0nEUliDVw",
-      authDomain: "cafecoffeeday-4ff76.firebaseapp.com",
-      databaseURL: "https://cafecoffeeday-4ff76.firebaseio.com",
-      projectId: "cafecoffeeday-4ff76",
-      storageBucket: "cafecoffeeday-4ff76.appspot.com",
-      messagingSenderId: "350483630600",
-      appId: "1:350483630600:web:c3124728318d931284dfbd"
-    };
-    // Initialize Firebase
-    firebase.initializeApp(firebaseConfig);
-  }
+  ngOnInit() {}
 }
